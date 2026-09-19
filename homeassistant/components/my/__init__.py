0f8e02f124932121765d3e1a47408e11f65007d9
Sebastian Werner <s.werner@sebastian-software.de>
2026-09-19 23:50:35 +0200

    headless: remove My panel registration

--- homeassistant/components/my/__init__.py
+++ homeassistant/components/my/__init__.py
@@ -1,6 +1,5 @@
 """Support for my.home-assistant.io redirect service."""
 
-from homeassistant.components import frontend
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
@@ -12,6 +11,5 @@ CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Register hidden _my_redirect panel."""
-    frontend.async_register_built_in_panel(hass, DOMAIN, frontend_url_path=URL_PATH)
+    """Set up the redirect integration without browser UI registration."""
     return True
