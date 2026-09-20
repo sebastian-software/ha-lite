eaab495aba84ef361ab9c55aa5c49bef80f6db4e
Sebastian Werner <s.werner@sebastian-software.de>
2026-09-20 13:31:15 +0200

    logbook: remove automation and script event coupling

--- homeassistant/components/logbook/const.py
+++ homeassistant/components/logbook/const.py
@@ -1,8 +1,6 @@
 """Event parser and human readable log generator."""
 
-from homeassistant.components.automation import EVENT_AUTOMATION_TRIGGERED
 from homeassistant.components.event import EventEntityStateAttribute
-from homeassistant.components.script import EVENT_SCRIPT_STARTED
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.const import EVENT_CALL_SERVICE, EVENT_LOGBOOK_ENTRY
 
@@ -46,8 +44,8 @@ LOGBOOK_ENTRY_WHEN = "when"
 # State attributes surfaced in logbook entries; extend as needed.
 EXPOSED_STATE_ATTRIBUTES = {EventEntityStateAttribute.EVENT_TYPE}
 
-# Automation events that can affect an entity_id or device_id
-AUTOMATION_EVENTS = {EVENT_AUTOMATION_TRIGGERED, EVENT_SCRIPT_STARTED}
+# Automation/script product events were removed from ha-lite.
+AUTOMATION_EVENTS: set[str] = set()
 
 # Events that are built-in to the logbook or core
 BUILT_IN_EVENTS = {EVENT_LOGBOOK_ENTRY, EVENT_CALL_SERVICE}
